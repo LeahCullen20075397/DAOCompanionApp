1d829aa86d0ad245fbf2eabbc6af4513db7a0707
Leah Cullen <leahclcv@gmail.com>
2021-01-02 15:33:02 +0000

    Logging added
    AnkoLogger added

--- app/src/main/java/ie/wit/daocompanionapp/PlaythroughActivity.kt
+++ app/src/main/java/ie/wit/daocompanionapp/PlaythroughActivity.kt
@@ -2,10 +2,13 @@ package ie.wit.daocompanionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class PlaythroughActivity : AppCompatActivity() {
+class PlaythroughActivity : AppCompatActivity(), AnkoLogger {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playthrough)
+        info("Playthrough Activity started...")
     }
 }
